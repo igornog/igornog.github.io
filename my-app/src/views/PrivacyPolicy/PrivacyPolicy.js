fa7139445109d74ecc3db8873aa11c63f255a0ee
Igor Nogueira <igorcmnog@gmail.com>
2021-03-21 13:36:10 -0500

    added privacy-policy page

---
+++ my-app/src/views/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,187 @@
+import "./privacypolicy.scss";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/Footer";
+import { lime } from "@material-ui/core/colors";
+
+function PrivacyPolicy() {
+  return (
+    <div className="App privacy-policy">
+      <Header />
+      <section>
+        <h2>Política de privacidade<br/> e cookies da Vitalicia</h2>
+        <p>
+          Esta política de privacidade e cookies se aplica ao processamento de
+          seus dados pessoais como usuário do aplicativo Vitalicia e do site
+          www.vitalicia.io. Esta política de privacidade e cookies não rege os
+          termos e condições em relação ao uso do Vitalicia ou os dados enviados
+          por você através do Vitalicia a outros usuários. Os termos e condições
+          para o uso de Vitalicia e o processamento de mensagens são regidos por
+          nosso Contrato de Licença de Usuário Final.
+        </p>
+        <p>
+          A Vitalicia leva a privacidade e segurança de dados muito a sério.
+          Tomamos todas as medidas técnicas usuais para prevenir o acesso não
+          autorizado aos seus dados em nossos servidores. Para toda a clareza no
+          que diz respeito às mensagens enviadas através de Siilo; como todos os
+          dados enviados pelos usuários são criptografados e apenas legíveis
+          pelo dispositivo de cada receptor, nenhuma outra parte, incluindo o
+          Siilo, pode descriptografar qualquer um desses dados. Finalmente,
+          nunca iremos vender ou fornecer seus dados pessoais a terceiros sem o
+          seu consentimento, exceto quando exigido por lei.
+        </p>
+
+        <h4>Fornecer seus dados pessoais não é obrigatório</h4>
+        <p>
+        O fornecimento de dados pessoais nunca é obrigatório. Você sempre pode decidir se deseja ou não fornecer dados pessoais. 
+No entanto, para poder fazer uso de alguns dos nossos serviços, é necessário fornecer dados pessoais. Se solicitarmos a inserção de dados pessoais, indicaremos quais dados são necessários para fazer uso do serviço e, portanto, devem ser fornecidos, e quais dados podem ser fornecidos opcionalmente.
+        </p>
+
+        <h4>Quais dados pessoais processamos?</h4>
+        <p>
+          Quando fornecemos nossos produtos e serviços, podemos processar seus
+          dados pessoais. Isso pode incluir os seguintes dados pessoais:
+        </p>
+        <p>
+          seu nome seu (s) endereço (s) comercial (is), número (s) de telefone e
+          endereço (s) de e-mail sua profissão, número de registro médico e
+          outras informações fornecidas em seu perfil (as outras informações são
+          opcionais) fotos (incluindo sua foto de perfil) enviadas por você
+          (opcional) dados sobre seus interesses e preferências como usuário de
+          nosso site, aplicativo, produtos e serviços (opcional) dados
+          financeiros relacionados a pedidos e pagamentos para nós e para você
+          dados relativos à sua visita ao nosso site e aplicativo, incluindo
+          URL, endereço IP, tipo de navegador, idioma, data, hora e duração da
+          sua visita dados sobre o status e a identidade do seu dispositivo
+          móvel Números de telefone do catálogo de endereços do telefone
+          (opcional)
+        </p>
+
+        <h4>Dados da Mensagem</h4>
+        <p>
+          Por padrão, quando os usuários enviam mensagens pela Vitalicia,
+          podemos armazenar temporariamente as mensagens e outros dados
+          relacionados (“Dados da Mensagem”) para garantir a entrega a todos os
+          destinatários. Após a entrega, os Dados da Mensagem serão excluídos de
+          nossos servidores e ficarão disponíveis apenas no dispositivo do
+          remetente e destinatário. Os usuários podem optar por usar serviços
+          adicionais do Vitalicia (por exemplo, Vitalicia Web Messenger) que
+          exigem, para seu correto funcionamento, que os Dados da Mensagem do
+          usuário também sejam armazenados em nossos servidores.
+        </p>
+        <p>
+          Por padrão, seus dados de mensagem ficarão indisponíveis após 30 dias
+          ou mais, se você ou sua organização tiver escolhido um período
+          alternativo. Seus dados de mensagem serão excluídos permanentemente de
+          nossos servidores após um período de retenção razoável limitado
+          necessário, exigido por razões de backup do sistema. Após a exclusão,
+          os dados da mensagem não podem ser recuperados. Os dados da mensagem
+          mantidos por outros usuários são regidos pela mesma política.
+        </p>
+        <p>
+          As mensagens são criptografadas de ponta a ponta. A Vitalicia não tem
+          como descriptografar mensagens de usuários porque não tem conhecimento
+          de suas chaves privadas. As chaves privadas dos usuários nunca são
+          armazenadas em nossos servidores. As mensagens e mídias criptografadas
+          (imagens, vídeos etc.) não são retidas mais do que o necessário para o
+          funcionamento correto da Vitalicia.
+        </p>
+        <p>
+          Embora as mensagens sejam enviadas e recebidas com segurança por meio
+          do Serviço, é em todos os casos e sempre da responsabilidade do
+          usuário determinar se o (s) destinatário (s) está / estão autorizado
+          (s) a receber quaisquer informações possivelmente confidenciais de
+          privacidade, como por exemplo informações médicas redutíveis para um
+          paciente específico. Os usuários são, portanto, responsáveis ​​pelas
+          informações que enviam a outros usuários. A Siilo não pode ser
+          responsabilizada ou responsabilizada por quaisquer informações
+          confidenciais de privacidade enviadas por meio do Serviço por usuários
+          sem a autorização necessária.
+        </p>
+
+        <h4>Com que finalidade processamos seus dados pessoais?</h4>
+        <p>
+          Seus dados pessoais podem ser processados ​​para os seguintes fins:
+        </p>
+        <p>
+          para fornecer a você acesso ao nosso site e aplicativo, com base ou
+          não em seu registro como usuário para mostrar quais de suas conexões
+          de seu catálogo de endereços ou lista de contatos também use a
+          Vitalicia para a celebração e implementação de um acordo concluído com
+          você para lhe fornecer e se adaptar às suas preferências em relação
+          aos serviços, produtos e / ou informações acordados para lhe enviar um
+          boletim informativo, informações do usuário ou uma mensagem de serviço
+          para permitir que você visualize e compartilhe com outras pessoas,
+          incluindo sua organização, dados do usuário no aplicativo ou no site
+          para permitir a interação com outros usuários e para convidar outras
+          pessoas a fazerem uso do Siilo para permitir que você forneça e troque
+          informações no site ou no aplicativo para melhorar a qualidade,
+          segurança e usabilidade do nosso site e aplicativo e para combater a
+          fraude para cumprir as regras e regulamentos impostos a nós e para
+          lidar com disputas Na medida em que o processamento listado acima
+          requer sua permissão, solicitaremos essa permissão com antecedência.
+          Você sempre pode revogar essa permissão.
+        </p>
+        <p>
+          Seus dados pessoais não serão usados ​​para outros fins que não os
+          listados acima.
+        </p>
+
+        <h4>Cookies</h4>
+        <p>
+          Cookies são pequenos arquivos de texto que são armazenados pelo seu
+          navegador. A seguir, explicamos resumidamente quais cookies usamos e
+          não usamos em nosso site e qual é sua finalidade.
+        </p>
+        <p>
+          Os cookies funcionais armazenam dados anônimos simples para permitir
+          que funções como login no site funcionem corretamente. Esses cookies
+          são necessários para que os aplicativos da web modernos funcionem de
+          maneira adequada e segura.
+        </p>
+        <p>
+          Os cookies analíticos recolhem estatísticas dos utilizadores do site,
+          para nos permitir melhorar constantemente a sua experiência de
+          utilizador. Esses cookies não afetam diretamente a experiência do
+          usuário e muitos navegadores fornecem um mecanismo para desativá-los.
+          Se você deseja bloquear cookies, ajuste as configurações do seu
+          navegador.
+        </p>
+        <p>
+          Os cookies entre sites / domínios são usados ​​para identificar um
+          usuário em vários sites para fins de rastreamento. A Vitalicia pode
+          receber esses cookies de sites de terceiros para determinar como os
+          usuários chegaram em nosso site. No entanto, nosso site não coloca
+          esses tipos de cookies no computador dos usuários para rastreá-los
+          depois que eles saem do site Siilo.
+        </p>
+        <p>
+          Você pode excluir os cookies que já estão instalados. Como ajustar
+          suas configurações varia de acordo com o navegador. Consulte a função
+          de ajuda do seu navegador.
+        </p>
+
+        <h4>Segurança e retenção</h4>
+        <p>
+          Tomamos medidas técnicas e organizacionais adequadas para proteger
+          seus dados pessoais contra perda ou qualquer forma de processamento
+          ilegal. Não reteremos ou manteremos seus dados por mais tempo do que o
+          permitido por lei, exigido por lei e / ou necessário para os fins para
+          os quais os dados são processados. O período de retenção, portanto,
+          depende da natureza dos dados e das finalidades para as quais os dados
+          são processados. Os períodos de retenção podem variar de acordo.
+        </p>
+
+        <h4>Alteração e versão</h4>
+        <p>
+          Podemos alterar esta política de privacidade e cookies. Recomendamos
+          que você verifique regularmente se há alterações. Esta política de
+          privacidade e cookies foi alterada pela última vez em 20 de março de
+          2021.
+        </p>
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
