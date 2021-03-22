30a59b50827577b718ab8d0fdc9acd80f690f73c
Igor Nogueira <igorcmnog@gmail.com>
2021-03-22 15:01:52 -0500

    fixed hover

--- my-app/src/sub-components/reason/reason.js
+++ my-app/src/sub-components/reason/reason.js
@@ -3,12 +3,12 @@ import React from 'react';
 import './reason-section.scss';
 import './reason-section-mb.scss';
 
-const reasonSection = ({title, subtitle, subtitle2, img, alt, index}) => 
+const reasonSection = ({title, title2, subtitle, subtitle2, img, alt, index}) => 
   <section className="reason-section">
       <article>
         <div>
             <span>{index}</span>
-            <h4>{title}</h4>
+            <h4>{title}<br/>{title2}</h4>
         </div>
         <p>{subtitle}</p>
         <p>{subtitle2}</p>
