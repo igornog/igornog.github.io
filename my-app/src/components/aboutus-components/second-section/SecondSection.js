93182c83dc775e46755360c5f2fdd5c710d59d34
Igor Nogueira <igorcmnog@gmail.com>
2021-03-29 12:51:03 -0500

    terms & privacy pages on mobile are done

--- my-app/src/components/aboutus-components/second-section/SecondSection.js
+++ my-app/src/components/aboutus-components/second-section/SecondSection.js
@@ -20,16 +20,14 @@ const thirdSection = ({title, title2, subtitle}) =>
     <div className="content">
       <FounderCard
         title="Luis Albinati"
-        subtitle="Sem mais confusão na hora de atender seus pacientes online. Com a Vitalícia você centraliza a comunicação com seus pacientes e mantém seus registros clínicos organizados, de maneira 100% segura e em um só lugar."
-        subtitleStrong=" Feito para as suas necessidades!"
+        subtitle="Luis Albinati é engenheiro, deixou a Microsoft para iniciar a Vitalicia em outubro de 2019 com a motivação de democratizar o acesso das pessoas às suas informações de saúde. Possui experiência técnica com mais de 12 anos em desenvolvimento de software e 8 anos dedicados a vendas e parcerias com passagem por empresas como IBM, Ericsson, Red Hat, SAP e Oracle. Luis também foi Country Manager da alemã EXASOL e advisor estratégico de algumas empresas."
         img={luisalbinatiImg}
         alt="Luis Albinati"
         link="https://www.linkedin.com/in/luisalbinati/"
       />
       <FounderCard
         title="Danilo Gaspar"
-        subtitle="Sem mais confusão na hora de atender seus pacientes online. Com a Vitalícia você centraliza a comunicação com seus pacientes e mantém seus registros clínicos organizados, de maneira 100% segura e em um só lugar."
-        subtitleStrong=" Feito para as suas necessidades!"
+        subtitle="Danilo Gaspar é administrador por formação e com pós em Gestão de Negócios, iniciou a carreira em Venture Capital, e tendo também passagem pelo Banco Original, Grupo Votorantim e recentemente atuou 3 anos como investidor de startups de saúde no Hospital Albert Einstein. Ao lado do Luis, em 2019, fundou a Vitalicia. Atualmente, é responsável por produto, gestão financeira e relacionamento com investidores."
         img={danilogasparImg}
         alt="Danilo Gaspar"
         link="https://www.linkedin.com/in/danilo-gaspar-7a48a563/"
